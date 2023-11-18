552d61ee28e99641734e7a65482e8b3639a616a7
Momo Eissenhauer <momo.eissenhauer@gmail.com>
2023-11-18 22:56:01 +0100

    add potatoes

--- production_chain/__main__.py
+++ production_chain/__main__.py
@@ -4,7 +4,8 @@ from production_chain import production_chain, visualize
 game_data = production_chain.load_game_data()
 
 
-chain = production_chain.compute_chains_for_facility("Bot Assembler", 4, game_data.folktails)
+# chain = production_chain.compute_chains_for_facility("Bot Assembler", 4, game_data.folktails)
+chain = production_chain.compute_chains_for_product("Biofuel", 16 + fractions.Fraction("2/3"), game_data.folktails)[0]
 
 print(chain)
 visualize.directed_graph(chain).view(cleanup=True)
